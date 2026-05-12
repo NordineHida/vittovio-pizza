0eace19bc19677b3b975de62043feedf712f7707
Nordine <nordine.hida@gmail.com>
2026-05-12 14:21:40 +0200

    Ajout du routeur et des routes

--- src/react/application/application.tsx
+++ src/react/application/application.tsx
@@ -1,16 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router";
 import { Header } from "../header/header";
 import { Sidebar } from "../sidebar/sidebar";
 import { ViewIngredients } from "../views/view-ingredients/view-ingredients";
+import { ViewDashboard } from "../views/view-dashboard/view-dashboard";
+import { ViewMenu } from "../views/view-menu/view-menu";
+import { ViewOrders } from "../views/view-orders/view-orders";
 
-export function Application()
-{
+export function Application() {
     return (
         <>
             <Header />
 
             <div className="content">
+                <BrowserRouter>
                     <Sidebar />
-                    <ViewIngredients />
+                    <Routes>
+                        <Route path="/" element={<ViewDashboard />} />
+                        <Route path="/menu" element={<ViewMenu />} />
+                        <Route path="/ingredients" element={<ViewIngredients />} />
+                        <Route path="/orders" element={<ViewOrders />} />
+                    </Routes>
+                </BrowserRouter>
+
             </div>
         </>
     )
